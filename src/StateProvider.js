36fbd37a7b6f713e0b7f2cda733df7f39b4370c1
oleggkam <milleroleg99@yandex.ru>
2020-09-17 21:44:56 +0700

    React context api was added

---
+++ src/StateProvider.js
@@ -0,0 +1,16 @@
+// setup data layer
+import React, { createContext, useContext, useReducer } from 'react';
+
+//data layer
+export const StateContext = createContext();
+
+//provider
+export const StateProvider = ({ reducer, initialState, children }) => (
+  <StateContext.Provider value={useReducer(reducer, initialState)}>
+    {children}
+  </StateContext.Provider>
+);
+
+export const useStateValue = () => useContext(StateContext);
+
+export default useStateValue;
--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,7 +1,23 @@
 import React from 'react';
 import '../css/Product.css';
+import useStateValue from '../StateProvider';
 
-function Product({ title, image, rating, price }) {
+function Product({ id, title, image, rating, price }) {
+  const [{ basket }, dispatch] = useStateValue();
+
+  const addToBasket = () => {
+    dispatch({
+      type: 'ADD_TO_BASKET',
+      item: {
+        id,
+        title,
+        image,
+        rating,
+        price,
+      },
+    });
+  };
+
   return (
     <div className="product">
       <div className="product__info">
@@ -19,7 +35,9 @@ function Product({ title, image, rating, price }) {
         </div>
       </div>
       <img className="product__image" src={image} alt="product__image" />
-      <button className="product__button">Add to basket</button>
+      <button className="product__button" onClick={addToBasket}>
+        Add to basket
+      </button>
     </div>
   );
 }
